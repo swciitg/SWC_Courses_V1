68aca03c2c9b8f6d8fca921ad38f3950afe712d2
yashsinghal10 <68996806+yashsinghal10@users.noreply.github.com>
2020-10-12 20:57:42 +0530

    Oauth-Outlook

--- client/src/components/LandingPage/LoginModal/LoginModal.js
+++ client/src/components/LandingPage/LoginModal/LoginModal.js
@@ -34,31 +34,8 @@ class LoginModal extends Component {
     }
     render() {
         return (
-            <div>
-                <Modal isOpen={this.state.modal} toggle={this.toggle}>
-                    <ModalHeader toggle={this.toggle}>
-                        Login to existing account
-                    </ModalHeader>
-                    <ModalBody toggle={this.toggle}>
-                        <Form onSubmit={this.onSubmit}>
-                            <FormGroup>
-                                <Label for="item">Email</Label>
-                                <Input type="email" name="email" id="email" onChange={this.onChange} />
-                                <small className="form-text text-muted">Enter your registered IITG email with @iitg.ac.in</small>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="item">Password</Label>
-                                <Input type="password" name="password" id="password" onChange={this.onChange} />
-                                <Button
-                                    block
-                                    color="dark"
-                                    style={{ margin: '2rem 0' }}
-                                >Login</Button>
-                            </FormGroup>
-                        </Form>
-                    </ModalBody>
-                </Modal>
-                <Button outline onClick={this.toggle} name="loginModal" className="btn btn-outline-dark btn-lg px-5 home-nav-button d-none d-md-inline" style={{ 'margin': '9px' }}>Login</Button>
+          <div>
+          <a href="/api/auth/outlook" class="button">Login</a>
             </div>
         )
     }
--- routes/auth2.routes.js
+++ routes/auth2.routes.js
@@ -5,6 +5,23 @@ const { authenticate } = require('../middleware/index')
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+app.get('/auth/outlook',
+  passport.authenticate('windowslive', {
+    scope: [
+      'openid',
+      'profile',
+      'offline_access',
+      'https://outlook.office.com/Mail.Read'
+    ]
+  })
+);
+
+app.get('/auth/outlook/callback',
+  passport.authenticate('windowslive', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 router.get("/user", authenticate, async (req, res) => {
     return res.send(req.user)
 });
