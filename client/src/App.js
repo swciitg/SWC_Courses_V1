617814a1623404705ea372696e85977adf178988
bhargav-saikia <bhargav.saikia@iitg.ac.in>
2020-12-13 10:21:13 +0530

    Custom VideoRoute and Streaming page structured

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styles from "./App.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "video.js/dist/video-js.css";
 import axios from "axios";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import CoursePage from "./components/CoursePage/CoursePage";
@@ -9,6 +10,8 @@ import CourseDetail from "./components/CourseDetail/CourseDetail";
 import Courses from "./components/Courses/Courses";
 import { AuthProvider } from "./contexts/AuthContext";
 import ProtectedRoute from "./hoc/ProtectedRoute";
+import VideoRoute from "./hoc/VideoRoute";
+import CourseVideos from "./components/CourseVideos/CourseVideos";
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
@@ -35,6 +38,16 @@ const App = (props) => {
               component={Courses}
             />
             <Route path="/courses/:id" exact component={CourseDetail} />
+            <Route
+              path="/courses/:courseId/videos/:id"
+              exact
+              component={CourseVideos}
+            />
+            {/* <VideoRoute
+              exact
+              path="/courses/:courseId/videos/:id"
+              component={CourseVideos}
+            ></VideoRoute> */}
           </Switch>
         </div>
       </BrowserRouter>
---
+++ client/src/components/CourseVideos/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from "react";
+import videojs from "video.js";
+
+function VideoPlayer(props) {
+  const playerRef = useRef();
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    sources: [
+      {
+        src: "https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd",
+        type: "application/dash+xml",
+      },
+    ],
+  };
+
+  useEffect(() => {
+    const player = videojs(
+      playerRef.current,
+      videoJsOptions,
+      function onPlayerReady() {
+        console.log("Video.js Ready", player);
+      }
+    );
+
+    return () => {
+      player.dispose();
+    };
+  });
+
+  return (
+    <div data-vjs-player>
+      <video ref={playerRef} className="video-js" playsInline />
+    </div>
+  );
+}
+
+export default VideoPlayer;
