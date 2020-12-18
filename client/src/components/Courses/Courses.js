7b48834a3192f2ac20b3ccadfcebd22cc8809a6b
yashsinghal10 <68996806+yashsinghal10@users.noreply.github.com>
2020-12-18 19:17:09 +0530

    all courses

--- client/src/components/Courses/Courses.js
+++ client/src/components/Courses/Courses.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
+import { Container } from "reactstrap";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-function Courses() {
-  return (
-    <div>
-      <p>All courses</p>
-    </div>
-  );
+class Courses extends Component  {
+  state = {
+     courses: [{title:"hdh"}]
+   };
+
+  componentDidMount = () => {
+     this.GetCourses();
+   };
+
+
+   GetCourses = () => {
+     axios.get('/api/admin/courses')
+       .then((response) => {
+         const data = response.data;
+         this.setState({ courses: data });
+         console.log('Data has been received!!');
+       })
+       .catch(() => {
+         alert('Error retrieving data!!!');
+       });
+   }
+   render(){
+    const { courses } = this.state.courses;
+    const courseList = courses.map(course => {
+      return (
+        <div className="ninja" key={course.id}>
+          <div>Name: { course.title }</div>
+          <div>Age: { course.author }</div>
+          <div>Belt: { course.description }</div>
+        </div>
+      )
+    });
+    return (
+      <div className="ninja-list">
+        { courseList }
+      </div>
+    );
+  }
 }
+
 
 export default Courses;
