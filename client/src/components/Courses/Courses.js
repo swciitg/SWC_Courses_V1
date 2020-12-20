06bd2b433f8d608f8e1f50dac2ba62eb4949944e
yashsinghal10 <68996806+yashsinghal10@users.noreply.github.com>
2020-12-20 18:39:23 +0530

    all courses

--- client/src/components/Courses/Courses.js
+++ client/src/components/Courses/Courses.js
@@ -3,21 +3,21 @@ import { Container } from "reactstrap";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CourseDetail from "../CourseDetail/CourseDetail";
+import styles from "./Courses.module.css";
+import Button from "@material-ui/core/Button";
+import AppNavbar from "../CoursePage/AppNavbar/AppNavbar";
 
 class Courses extends Component  {
   state = {
-     courses: [{title:"hdh"}]
+     courses: []
    };
-
-  componentDidMount = () => {
-     this.GetCourses();
-   };
-
-
    GetCourses = () => {
      axios.get('/api/admin/courses')
        .then((response) => {
-         const data = response.data;
+         const data = response.data.courses;
          this.setState({ courses: data });
          console.log('Data has been received!!');
        })
@@ -25,21 +25,53 @@ class Courses extends Component  {
          alert('Error retrieving data!!!');
        });
    }
-   render(){
-    const { courses } = this.state.courses;
-    const courseList = courses.map(course => {
-      return (
-        <div className="ninja" key={course.id}>
-          <div>Name: { course.title }</div>
-          <div>Age: { course.author }</div>
-          <div>Belt: { course.description }</div>
+
+  componentDidMount = () => {
+     this.GetCourses();
+     console.log(this.state.courses);
+   };
+
+   displaycourselist = (courses) => {
+
+    if (!courses.length) return null;
+
+
+    return courses.map((course) => (
+      <div key={course._id} className="blog-post__display">
+      <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
+            <div class="card" id="cards">
+               <img class="card-img-top" src="https://picsum.photos/200/150/?random" />
+                <div class="card-block">
+                    <h3 class="card-title">{course.title}</h3>
+                    <div class="meta">
+                        <h5>{course.author}</h5>
+                    </div>
+                    <div class="card-text">
+                        <p>{course.description}</p>
+                    </div>
+                    <div>
+                     <Button size="small" color="primary">
+                       <Link to={{ pathname: `/courses/${course._id}`}}>
+                         Learn more
+                       </Link>
+                     </Button>
+                     </div>
+                </div>
+            </div>
         </div>
-      )
-    });
-    return (
-      <div className="ninja-list">
-        { courseList }
       </div>
+    ));
+  };
+
+
+   render(){
+    return (
+      <div className={styles.App}>
+        <AppNavbar />
+        <div className="blog" id="cards" >
+          {this.displaycourselist(this.state.courses)}
+        </div>
+        </div>
     );
   }
 }
