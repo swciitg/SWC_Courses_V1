ffc65035f78e7a6b1ced3e95caf99ee0c0ee60b6
yashsinghal10 <68996806+yashsinghal10@users.noreply.github.com>
2021-01-02 11:15:25 +0530

    search bar updated

--- client/src/components/Courses/Courses.js
+++ client/src/components/Courses/Courses.js
@@ -12,7 +12,8 @@ import AppNavbar from "../CoursePage/AppNavbar/AppNavbar";
 
 class Courses extends Component  {
   state = {
-     courses: []
+     courses: [],
+     inputvalue : ''
    };
    GetCourses = () => {
      axios.get('/api/admin/courses')
@@ -31,22 +32,35 @@ class Courses extends Component  {
      console.log(this.state.courses);
    };
 
+   filterchange = (event) => {
+     console.log("hello",event.target.value)
+     this.setState({
+       inputvalue: event.target.value
+     })
+   };
+
    displaycourselist = (courses) => {
+    const newcourses = this.state.courses.filter(course =>{
+      return course.title.toLowerCase().includes(this.state.inputvalue.toLowerCase())
+    });
+    if (!newcourses.length) return (
+      <div className={styles.nocourse}>
+      No Courses Found
+      </div>
+    );
 
-    if (!courses.length) return null;
 
-
-    return courses.map((course) => (
-      <div key={course._id} className="blog-post__display">
-      <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card" id="cards">
-               <img class="card-img-top" src="https://picsum.photos/200/150/?random" />
-                <div class="card-block">
-                    <h3 class="card-title">{course.title}</h3>
-                    <div class="meta">
+    return newcourses.map((course) => (
+      <div key={course._id} className={styles.blog}>
+      <div className="col-sm-6 col-md-4 col-lg-3 mt-4">
+            <div className="card" id="cards">
+               <img className="card-img-top" src="https://picsum.photos/200/150/?random" />
+                <div className="card-block">
+                    <h3 className="card-title">{course.title}</h3>
+                    <div className="meta">
                         <h5>{course.author}</h5>
                     </div>
-                    <div class="card-text">
+                    <div className="card-text">
                         <p>{course.description}</p>
                     </div>
                     <div>
@@ -70,6 +84,10 @@ class Courses extends Component  {
         <AppNavbar />
         <span>___________________________________________________________________________________________________________________________________________________________________________</span>
         <br/>
+        <div>
+        <label htmlFor="search"> SEARCH BY NAME</label>
+        <input type = "text" value = {this.state.inputvalue} onChange= {this.filterchange} placeholder = "" />
+        </div>
         <div className="blog" id="cards" >
           {this.displaycourselist(this.state.courses)}
         </div>
