a294b13d0552be1c77e8a7ca223853443cfe79a0
Sarthak Bhagwat <58619383+s-bhagwat@users.noreply.github.com>
2021-01-04 23:43:41 +0530

    added secion index and video index in videos
    now one can easily sort the videos according to their sectionIndex and videoIndex

--- models/media.js
+++ models/media.js
@@ -1,15 +1,15 @@
 var mongoose = require("mongoose");
-let Schema = mongoose.Schema
+let Schema = mongoose.Schema;
 
 let mediaSchema = new Schema({
-
-	title: { type: String },
-	filePath: { type: String },
-	course: { type: mongoose.ObjectId, ref: 'Course' },
-	thumbnail: { type: String },
-	//subtopic: { type: String, required: true},
-	viewcount: { type: Number, default: 0 },
-	duration: { type: Number }
-})
+  title: { type: String },
+  filePath: { type: String },
+  course: { type: mongoose.ObjectId, ref: "Course" },
+  thumbnail: { type: String },
+  //subtopic: { type: String, required: true},
+  viewcount: { type: Number, default: 0 },
+  duration: { type: Number },
+  index: { sectionIndex: { type: Number }, videoIndex: { type: Number } },
+});
 
 module.exports = mongoose.model("Media", mediaSchema);
