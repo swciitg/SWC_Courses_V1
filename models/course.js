03e57874e601a48b125450ac0d654c619e91db01
Sarthak Bhagwat <58619383+s-bhagwat@users.noreply.github.com>
2021-01-04 23:41:17 +0530

    added thumbnail image path and updated title

--- models/course.js
+++ models/course.js
@@ -2,11 +2,15 @@ let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 let courseSchema = new Schema({
-  title: { type: String, required: true },
+  title: {
+    type: String,
+    unique: [true, "Course Name is not unique"],
+    required: [true, "A course must have a name"],
+  },
   author: { type: String, required: true },
   topics: [String],
   description: { type: String, required: true },
-  img: { type: String, required:true },
+  imgPath: { type: String },
   videos: [{ type: mongoose.ObjectId, ref: "Media" }],
 });
 
