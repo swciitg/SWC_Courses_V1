59e71215b00a32a60822976e9c4e4fdfb9cd3b7f
Bhargav-saikia1999 <bhargavsaikia06@gmail.com>
2021-01-09 15:14:21 +0530

    details page minor change

--- client/src/components/CourseDetail/DashBoard/Content/Content.js
+++ client/src/components/CourseDetail/DashBoard/Content/Content.js
@@ -8,28 +8,11 @@ const Content = ({ details }) => {
       <div className={styles.Content_box}>
         <div className={classNames(styles.Header)}>
           <span>CONTENTS</span>
-          <span>30 Lessons</span>
-          <span>20 hrs</span>
+          <span>{details.videos.length} Lessons</span>
+          <span>_ hrs</span>
         </div>
         <div className={styles.lessons}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-          voluptatum molestias earum architecto ipsa harum dolor nostrum velit
-          totam facilis voluptates delectus dolorem laboriosam quos, libero fuga
-          fugit! Neque blanditiis atque quia facilis iste! Modi corrupti maxime,
-          soluta accusantium ut corporis magnam mollitia excepturi esse harum!
-          Similique porro, accusantium sunt officiis impedit laboriosam?
-          Eligendi sapiente quos et sunt. Eum unde quas laudantium error vitae,
-          alias dolore perferendis fugiat impedit totam eveniet quia, dolores
-          numquam. Nostrum aut molestias placeat exercitationem nemo officiis
-          consequuntur fugit harum corporis reiciendis voluptatibus labore
-          praesentium veritatis eligendi eveniet perferendis consectetur, soluta
-          libero repellendus maxime totam inventore? Explicabo inventore labore
-          nostrum aliquam a voluptatibus unde ex nesciunt minus iste? Sunt
-          quidem dicta nam, rem doloremque ipsa repellendus nobis atque quas,
-          dolorem consectetur unde necessitatibus adipisci eum ducimus minima
-          numquam exercitationem. Voluptate unde laboriosam qui ab atque? Cum
-          dolor cupiditate perferendis aliquid expedita ipsam laudantium
-          repellat accusantium inventore.
+          <p>ENROL-- GO TO COURSE-- GET STARTED</p>
         </div>
       </div>
       <div className={classNames(styles.Description)}>
