a44ba585c8241b5646b644fa3a0861dbf613f21d
Bhargav-saikia1999 <bhargavsaikia06@gmail.com>
2021-03-16 00:34:36 +0530

    minor changes

--- client/src/components/Courses/CardsContainer.js
+++ client/src/components/Courses/CardsContainer.js
@@ -10,6 +10,7 @@ import {
 import styles from "./Courses.module.css";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
+import Skeleton from "@material-ui/lab/Skeleton";
 
 const CardsContainer = (props) => {
   const { courses, newcourses, inputvalue } = props;
@@ -18,8 +19,25 @@ const CardsContainer = (props) => {
     const newcourses = courses.filter((course) => {
       return course.title.toLowerCase().includes(inputvalue.toLowerCase());
     });
-    if (!newcourses.length)
-      return <div className={styles.nocourse}>No Courses Found</div>;
+    if (!newcourses.length) {
+      return [1, 2, 3].map((val) => {
+        return (
+          <Skeleton
+            variant="rect"
+            width={256}
+            height={360}
+            style={{
+              width: "16rem",
+              height: "22.5rem",
+              padding: "1rem",
+              margin: "1rem",
+              borderRadius: "4px",
+            }}
+            key={val}
+          />
+        );
+      });
+    }
 
     return newcourses.map((course) => (
       <Link
