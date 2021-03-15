25845010d02d5b3bb1bdef10f3c42ec53615a47a
chelsi-001011 <57683338+chelsi-001011@users.noreply.github.com>
2021-03-15 17:20:57 +0530

    Course Details List

--- client/src/components/CourseDetail/DashBoard/Content/Content.js
+++ client/src/components/CourseDetail/DashBoard/Content/Content.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styles from "./Content.module.css";
 import classNames from "classnames";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const Content = ({ details }) => {
+
+  const [lessons, setLessons] = useState([]);
+  const [hrs,setLessonHrs] = useState("");
+  const [minutes,setLessonMin] = useState("");
+  useEffect(() => {
+    axios
+      .get(`/api/courses/${details.id}`)
+      .then((response) => {
+        const Lessons = response.data.media;
+        var time = 0; //seconds
+        setLessons(Lessons);
+        console.log(response.data.media);
+        // console.log(Lessons);
+        Lessons.forEach(lesson => {
+          time += Number(lesson.duration);
+        });
+        time = time/60; //minutes
+        var hours = (time / 60);
+        var rhours = Math.floor(hours);
+        var minutes = (hours - rhours) * 60;
+        var rminutes = Math.round(minutes);
+        setLessonHrs(rhours);
+        setLessonMin(rminutes);
+        console.log("Lessons has been received!!");
+      })
+      .catch(() => {
+        alert("Error retrieving Lessons!!");
+      });
+  }, []);
+
   return (
     <div className={classNames(styles.Content)}>
       <div className={styles.Content_box}>
@@ -11,10 +45,19 @@ const Content = ({ details }) => {
           <span>
             {details.videos !== undefined && details.videos.length} Lessons
           </span>
-          <span>_ hrs</span>
+          <span>{hrs == 0? minutes +" min" : hrs +" hrs"}</span>
         </div>
         <div className={styles.lessons}>
-          <p>ENROL-- GO TO COURSE-- GET STARTED</p>
+          <List className={styles.lesson}>
+            {lessons.map((lesson,index) => {
+
+        return (
+          <ListItem  style={index%2? {background:"#ececec"}: {background:"#ffffff"}} >
+            <ListItemText >{lesson.title}</ListItemText>
+          </ListItem>
+        );
+      })}
+          </List>
         </div>
       </div>
       <div className={classNames(styles.Description)}>
