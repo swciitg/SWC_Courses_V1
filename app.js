c933c930565652a4c7f1cf3ab19118aa05c54059
Sarthak <sarthak.bhagwat@rediffmail.com>
2021-03-17 15:02:29 +0530

    dep

--- app.js
+++ app.js
@@ -90,10 +90,23 @@ app.use((err, req, res, next) => {
   }
 });
 
-///// CHANGE THIS BEFORE DEPLOYMENT
-app.get("*", function (req, res) {
-  res.send("PAGE NOT FOUND 404");
-});
+// ///// CHANGE THIS BEFORE DEPLOYMENT
+// app.get("*", function (req, res) {
+//   res.send("PAGE NOT FOUND 404");
+// });
+
+//Setup routes
+// Serve static assets if in production
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  console.log("i'm running");
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, function () {
   console.log(`SWC Media server has started at port ${PORT}`);
