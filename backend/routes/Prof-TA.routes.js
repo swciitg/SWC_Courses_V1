4b950733e2cbf2289b6be8850c282b4d0983c236
komalsatwik <komalsatwik2002@gmail.com>
2021-06-24 13:31:26 +0530

    router,controller updated

--- backend/routes/Prof-TA.routes.js
+++ backend/routes/Prof-TA.routes.js
@@ -4,8 +4,8 @@ const passport = require("passport");
 const usercontroller = require("../controllers/user.controller")
 const { CLIENT_HOME_PAGE_URL } = process.env;
 
-router.get("/Profs",usercontroller.getAllProfs);
-router.get("/TAs",usercontroller.getAllTAs);
+router.get("/profs",usercontroller.getAllProfs);
+router.get("/tas",usercontroller.getAllTAs);
   router.get("/prof/:id", usercontroller.getProf);
   router.post("/prof/post", usercontroller.postProf);
   router.put("/prof/:id", usercontroller.editProf);
