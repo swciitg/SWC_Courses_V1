b8163445ece84f7fb2c6653692e00fd6c9a1f0e2
Abhijeet Veer <veerabhijeet10@gmail.com>
2021-06-28 18:12:18 +0530

    client/src/components/admin/Calender/Days.js

---
+++ client/src/components/admin/Calender/Days.js
@@ -0,0 +1,61 @@
+//import './Single'
+import Single from "./Single";
+
+
+const DaysOfWeek = [
+    {
+      id: "1",
+      name: "Sunday",
+      
+      
+    },
+    {
+      id: "2",
+      name: "Monday",
+      
+    },
+    {
+      id: "3",
+      name: "Tuesday",
+      
+    },
+    {
+      id: "4",
+      name: "Wednesday",
+      
+    },
+    {
+      id: "5",
+      name: "Thursday",
+      
+    },
+    {
+      id: "6",
+      name: "Friday",
+      
+    },
+    {
+      id: "7",
+      name: "Saturday",
+      
+    },
+    
+  ];
+
+function createDays(loop) {
+    return (
+      <Single
+        key={loop.id}
+        name={loop.name}
+      />
+    );
+  }
+
+const Days=()=>{
+    return(
+        <div className="flex  mb-10 flex-row gap-5 justify-center h-144">
+            {DaysOfWeek.map(createDays)} 
+        </div>
+    );
+}
+export default Days;
