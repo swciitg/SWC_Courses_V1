14ead1fa43b07f89b8f80ebb8b97f237d8bb2f90
walkingn8mare <ashish18a@iitg.ac.in>
2021-07-04 13:42:25 +0530

    fixes

--- backend/routes/video.routes.js
+++ backend/routes/video.routes.js
@@ -5,18 +5,18 @@ const videoController = require('../controllers/video.controller');
 const auth = require('../middlewares/auth'); 
 
 // Get details of all the course videos in an array
-router.get('/:course_id',auth.isLoggedIn, videoController.videoInfo);
+//router.get('/:course_id',auth.isLoggedIn, videoController.videoInfo);
 
 // Upload a video (admin only)
 router.post('/:course_id',auth.isAdmin, videoController.uploadVideo);
 
 // Get the thumbnail of a video 
-router.get('/thumbnail/:video_id',auth.isLoggedIn, videoController.getThumbnail);
+//router.get('/thumbnail/:video_id',auth.isLoggedIn, videoController.getThumbnail);
 
 // Get a video 
-router.get('/:video_id', auth.isLoggedIn, videoController.getVideo);
+//router.get('/:video_id', auth.isLoggedIn, videoController.getVideo);
 
 // Delete video (admin only)
-router.delete('/:video_id', auth.isAdmin, videoController.deleteVideo);
+//router.delete('/:video_id', auth.isAdmin, videoController.deleteVideo);
 
 module.exports = router;
